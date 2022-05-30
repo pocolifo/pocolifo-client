38c78f710be36115316f23310d4d01253ee77908
youngermax <youngermax01@gmail.com>
2022-05-30 07:36:36 -0400

    Apply shellcheck

--- upload.sh
+++ upload.sh
@@ -1,6 +1,7 @@
+# shellcheck disable=SC2044
 for file in $(find ./build/tmp/generateVersionPublishFiles -type f); do
   upload_path=${file/"./build/tmp/generateVersionPublishFiles/"/}
   echo "Upload $upload_path"
-  curl https://maven.services.pocolifo.com/pocolifoclient/v2/$upload_path -u ${REPO_USERNAME}:${REPO_PASSWORD} --upload-file $file
+  curl https://maven.services.pocolifo.com/pocolifoclient/v2/"$upload_path" -u "${REPO_USERNAME}":"${REPO_PASSWORD}" --upload-file "$file"
   echo ""
 done
